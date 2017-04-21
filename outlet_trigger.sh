1b1dcf1c55f044057aa1aef8650217f813443898
Ethan Cassel-Mace <casselmacee@carleton.edu>
2017-04-21 22:04:48 +0000

    shortened time

--- outlet_trigger.sh
+++ outlet_trigger.sh
@@ -3,7 +3,7 @@
 # 0 is circuit-made  1 is circuit broken
 echo out > /sys/class/gpio/gpio$1/direction
 echo 0 > /sys/class/gpio/gpio$1/value
-sleep 1
+sleep 0.07
 echo 1 > /sys/class/gpio/gpio$1/value
 
 
