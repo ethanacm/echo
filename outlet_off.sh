bce63f3d09a7b867151f8bf71cb8564870459854
Ethan Cassel-Mace <casselmacee@carleton.edu>
2017-04-21 16:59:45 -0500

    added

--- outlet_off.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# press the off button for a second and then release
-# 0 is circuit-made  1 is circuit broken
-echo out > /sys/class/gpio/gpio$1/direction
-echo 0 > /sys/class/gpio/gpio$1/value
-sleep 1
-echo 1 > /sys/class/gpio/gpio$1/value
