3796ce676ce8c25b52f177d936904eafab01d50b
Ethan Cassel-Mace <casselmacee@carleton.edu>
2017-04-22 16:17:25 -0500

    added stuff

--- outlet_handler.py
+++ outlet_handler.py
@@ -8,13 +8,13 @@ class OutletHandler:
             os.system("sudo sh outlet_setup.sh " + str(self.gpio_dict[key][0]) + " " + str(self.gpio_dict[key][1])) #setup all outlets
 
     def turn_on(self, outlet_num):
-        if outlet_num in range(0, len(self.gpio_dict)):
+        if outlet_num in range(1, len(self.gpio_dict) + 1):
             os.system("sudo sh outlet_trigger.sh " + str(self.gpio_dict[outlet_num][0]))
         else:
             print("INVALID OUTLET")
 
     def turn_off(self, outlet_num):
-        if outlet_num in range(0, len(self.gpio_dict)):
+        if outlet_num in range(1, len(self.gpio_dict) + 1):
             os.system("sudo sh outlet_trigger.sh " + str(self.gpio_dict[outlet_num][1]))
         else:
             print("INVALID OUTLET")
