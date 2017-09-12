fa26cce51a980817dc9099fd62bc864dce47d9e8
Ethan Cassel-Mace <casselmacee@carleton.edu>
2017-09-11 21:47:04 -0500

    increase gpio on time

--- outlet_trigger.sh
+++ outlet_trigger.sh
@@ -3,7 +3,5 @@
 # 0 is circuit-made  1 is circuit broken
 echo out > /sys/class/gpio/gpio$1/direction
 echo 0 > /sys/class/gpio/gpio$1/value
-sleep 0.07
+sleep 0.2
 echo 1 > /sys/class/gpio/gpio$1/value
-
-
